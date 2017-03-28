2fe442bfc7e9a058ea14005c9ef6370a21c65187
Alex Polzin <Alex.D.Polzin@gmail.com>
2017-03-28 17:40:40 -0400

    ok for now

---
+++ News Wire Fix/Assets/Scripts/TextRe.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextRe : MonoBehaviour {
+
+    public Text text;
+    public string key;
+	// Use this for initialization
+	void Start () {
+        text.supportRichText = true;
+        text.text = text.text.Replace(key, "<color=blue>"+ key+ "</color>");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

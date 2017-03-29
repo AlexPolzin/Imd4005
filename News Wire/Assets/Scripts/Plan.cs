1044358b2fd3573ed4675aa7c97b72f3953b50b4
Alex Polzin <Alex.D.Polzin@gmail.com>
2017-03-29 15:35:08 -0400

    better array

--- News Wire/Assets/Scripts/Plan.cs
+++ News Wire/Assets/Scripts/Plan.cs
@@ -18,7 +18,10 @@ public class Plan : MonoBehaviour {
         timeline.Add(new EventDev.Events(2f, "Phone", "this is and event", "Mr. Phone",new string[] { " is " }, 0f));
         timeline.Add(new EventDev.Events(3f, "Radio", "this is and event2", "Mr. Radio", new string[] { " and " }, 0f));
         timeline.Add(new EventDev.Events(4f, "Tweet", "this is and event3", "Mr. Tweet", new string[] { " event " }, 0f, images[0]));
-	}
+        timeline.Add(new EventDev.Events(6f, "Phone", "phone 2", "Mr. Phone", new string[] { " is " }, 0f));
+        timeline.Add(new EventDev.Events(7f, "Radio", "radio 2", "Mr. Radio", new string[] { " and " }, 0f));
+        timeline.Add(new EventDev.Events(8f, "Tweet", "tweet 2", "Mr. Tweet", new string[] { " event " }, 0f, images[0]));
+    }
 	
 	// Update is called once per frame
 	void Update () {
